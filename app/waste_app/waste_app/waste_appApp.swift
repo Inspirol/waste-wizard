e6ccf80ca4aff3601c9e285a1eaa89d1062e3fef
Inspirol <91295122+Inspirol@users.noreply.github.com>
2024-02-19 11:12:58 -0500

    IOS swift

---
+++ app/waste_app/waste_app/waste_appApp.swift
@@ -0,0 +1,32 @@
+//
+//  waste_appApp.swift
+//  waste_app
+//
+//  Created by Eric Yang on 2024-02-14.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct waste_appApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
