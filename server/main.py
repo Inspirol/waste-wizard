e9522eb8c4a1a0235b98d9be52a05139d8e43a42
Inspirol <91295122+Inspirol@users.noreply.github.com>
2024-02-19 11:10:40 -0500

    edit server

--- server/main.py
+++ server/main.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, stream_with_context
 
 app = Flask(__name__)
 
@@ -10,6 +10,16 @@ def hello_world():
     """Example Hello World route."""
     name = os.environ.get("NAME", "World")
     return f"Hello {name}!"
+
+@app.route("/hi")
+def hi():
+    """Example Hi route."""
+    name = os.environ.get("NAME", "World")
+    return {
+        "message": f"Hi {name}!",
+    }
+    
+
 
 
 if __name__ == "__main__":
